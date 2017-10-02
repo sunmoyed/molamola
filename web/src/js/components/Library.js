1f2f98ecee223fdc1abe587a208182fb9c6a85b0
chris (daiyi) <daiyitastic@gmail.com>
2017-10-02 22:18:05 +0200

    Everyone into components

--- web/src/js/components/Library.js
+++ web/src/js/components/Library.js
@@ -2,51 +2,49 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Library extends Component {
-
   render() {
     return (
       <div className="library">
-        <div className="list">
-          <div className="row">
-            <div className="column">
-              title
-            </div>
-            <div className="column">
-              information
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="column">
-              I am an anime
-            </div>
-            <div className="column">
-              an image
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="column">
-              hello
-            </div>
-            <div className="column">
-              potato!
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="column">
-              I am an anime
-            </div>
-            <div className="column">
-              an image
-            </div>
-          </div>
-
-        </div>
+        <List rows={[ ["title", "info"],
+                      ["cowboy bebop", "fav"],
+                      ["penguindrum", "??? what even is"],
+                      ["psycho-pass", "I still need to finish this xD"] ]} />
       </div>
     );
   }
+}
+
+
+const List = ({ rows }) => (
+  <div className="list">
+    {rows.map((row, i) => (
+      <ListRow key={i} columns={row} />
+    ))}
+  </div>
+)
+
+const ListRow = ({ columns }) => (
+  <div className="row">
+    {columns.map((column, i) => (
+      <ListRowColumn key={i} text={column} />
+    ))}
+  </div>
+)
+
+const ListRowColumn = ({ text }) => (
+    <div className="column">
+      {text}
+    </div>
+)
+
+List.propTypes = {
+  rows: PropTypes.array
+}
+ListRow.propTypes = {
+  columns: PropTypes.array
+}
+ListRowColumn.propTypes = {
+  text: PropTypes.string
 }
 
 export default Library
